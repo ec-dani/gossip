ccacbd2bf3c113aecd419cf282814bc4940963d3
ec-dani <jd.cantuna@alumnos.upm.es>
2020-10-29 12:05:03 +0100

    get data form

--- src/script.js
+++ src/script.js
@@ -1,10 +1,11 @@
 const url = "https://api.github.com/users/";
-const user ="Pochu15"
-const sub = url.length+user.length;
+//const user ="Pochu15"
+//const sub = url.length+user.length;
+//console.log(url+user,"sub "+sub);
 
-console.log(url+user,"sub "+sub);
-$(function(){
-  $("#see").click(function(){
+/*$(function(){
+  $("#user").click(function(){
+    console.log(document.querySelector("#user").value);
     $.get(url+user, function(data,status){
       console.log(data.login)
       console.log(data.avatar_url)
@@ -18,7 +19,26 @@ $(function(){
       })
     })
   });
-})
+});*/
+
+var user;
+$("#myForm").submit(function(event) {
+    event.preventDefault();
+    user= $(this).find('input[name="user"]').val();
+    console.log(user);
+    $.get(url+user, function(data,status){
+      console.log(data.login)
+      console.log(data.avatar_url)
+      console.log(data.followers)
+      console.log(data.following)
+      console.log(data.starred_url)
+      let stars = data.starred_url.substring(-1, url.length+user.length+8);
+      console.log(stars);
+      $.get(stars, function(datastars,status){
+        console.log("Estrellas: "+datastars.length);
+      })
+    })
+});
 
 
 /*
